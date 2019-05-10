4c39db28d32788a9f8acc20bd4ce9fdc011f79e1
notmarkmiranda <notmarkmiranda@gmail.com>
2019-05-10 14:42:16 -0600

    fix travel in specs

--- spec/features/users/user_can_sign_up_spec.rb
+++ spec/features/users/user_can_sign_up_spec.rb
@@ -26,7 +26,7 @@ describe 'sign up and confirm flow', type: :feature do
   end
 
   describe 'after creating, but before confirming' do
-    let(:user) { create(:user, :unconfirmed) }
+    let(:user) { create(:user, :unconfirmed, confirmation_expiration: Time.now) }
 
     describe 'with the correct confirmation number' do
       it 'allows a user to confirm themselves' do
@@ -52,8 +52,10 @@ describe 'sign up and confirm flow', type: :feature do
     end
 
     describe 'with an expired confirmation number' do
+
       it 'does not allow a user to confirm' do
-        travel_to (DateTime.parse(user.confirmation_expiration) + 10.minutes) do
+        user
+        travel 31.minutes do
           visit confirm_user_path(user)
 
           fill_in 'Enter Confirmation Code!', with: user.confirmation_number
